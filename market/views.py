d63f37afa83814a1195c4c5b6fdddb50469b8efc
Silian1234 <isilinkina25565137@gmail.com>
2025-06-13 00:41:42 +0500

    Fixes

--- market/views.py
+++ market/views.py
@@ -206,10 +206,6 @@ class RegisterView(APIView):
 
         user = User.objects.create_user(username=username, role=role, is_staff=is_staff)
 
-        # стартовый бонус
-        BALANCES[str(user.id)]["RUB"] = Decimal("250")
-        Account.objects.get_or_create(user=user, defaults={"balance": 250})
-
         data = {
             "id": str(user.id),
             "name": user.username,
